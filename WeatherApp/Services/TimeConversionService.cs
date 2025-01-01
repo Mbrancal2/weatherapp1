cbcc1cb52771e0dbc4d89ccbc9bc12057aee1ea9
Mbrancal2 <124226460+Mbrancal2@users.noreply.github.com>
2024-12-31 20:23:43 -0500

    Update TimeConversionService.cs
    removed some comments and commented out some Console.writeLine()s used for testing

--- WeatherApp/Services/TimeConversionService.cs
+++ WeatherApp/Services/TimeConversionService.cs
@@ -9,21 +9,11 @@ public class TimeConversionService : ITimeConversionService
         DateTime utcDateTime = datetimeOffset.UtcDateTime; 
         // Add the timezone offset to get the local DateTime
         DateTime localDateTime = utcDateTime.AddSeconds(timezone);
-        Console.WriteLine(unixtime);
-        Console.WriteLine(timezone);
+        //Console.WriteLine(unixtime);
+        //Console.WriteLine(timezone);
         // Return the local DateTime 
         return localDateTime;
-        //adds up given unixtime + number of seconds difference from timezone
-        //long total_Seconds = unixtime + (timezone * 60);
-         //DateTimeOffset datetimeoffset = DateTimeOffset.FromUnixTimeSeconds(unixtime);
-         //DateTime utctim = datetimeoffset.UtcDateTime;
-
-        //DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(unixtime); 
-        //TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById(timezone); 
-        ///DateTime localTime = TimeZoneInfo.ConvertTime(dateTimeOffset.UtcDateTime, timeZone); 
-        ///
-         //Console.WriteLine(utctim.AddSeconds(timezone));
-         //return utctim.AddSeconds(timezone);
+        
     }
     
 }
